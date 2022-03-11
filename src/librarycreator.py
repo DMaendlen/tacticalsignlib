e875cf5152de5a6e747c806eff9a7f5e23352bf2
David Mändlen <david.maendlen@bifroe.st>
2022-03-11 18:26:39 +0100

    improve after linting

--- src/librarycreator.py
+++ src/librarycreator.py
@@ -4,7 +4,7 @@
 
 from base64 import b64encode
 from io import BytesIO
-from os import listdir, path, remove, walk 
+from os import listdir, path, remove, walk
 from pathlib import Path
 from shutil import rmtree
 from urllib.request import urlopen
@@ -13,34 +13,33 @@ from zipfile import ZipFile
 targetdir  = Path('../libfiles')
 signdir = Path('taktischezeichen')
 
-def write_xmlstring_to_file(xmlstring: str, outfilename: str):
+def write_xmlstring_to_file(xml: str, outfile: str):
     """Write a given XML string to a file in the target directory"""
-    outfile = path.join(targetdir, outfilename)
-    with open(outfile, 'a') as out:
-        out.write(xmlstring)
+    outfile = path.join(targetdir, outfile)
+    with open(outfile, 'a', encoding='utf-8') as out:
+        out.write(xml)
 
-def create_b64string(path: Path):
+def create_b64string(filepath: Path):
     """Create a b64 string from a given file"""
-    with open(path, 'rb') as f:
-        encodedSVG = b64encode(f.read())
-        return encodedSVG.decode()
+    with open(filepath, 'rb') as readfile:
+        return b64encode(readfile.read()).decode()
 
-def create_jsonstring(b64string: str, title: str):
+def create_jsonstring(b64: str, signtitle: str):
     """Create the json string expected by draw.io / diagrams.net with a given
     b64 representation of a file and a title, width and height are fixed to 64px
     (can be dynamically changed in the diagram)"""
 
-    return f"""{{"data": "data:image/svg+xml;base64,{b64string}", "w": 64, "h": 64, "title": "{title}"}}"""
+    return f"""{{"data": "data:image/svg+xml;base64,{b64}", "w": 64, "h": 64, "title": "{signtitle}"}}"""
 
 # clean up previous instances of library files
 for f in listdir(targetdir):
     remove(path.join(targetdir, f))
 
 # Download and unzip latest release of tactical signs
-zipurl = 'https://github.com/jonas-koeritz/Taktische-Zeichen/releases/latest/download/release.zip'
-with urlopen(zipurl) as zipresp:
+ZIPURL = 'https://github.com/jonas-koeritz/Taktische-Zeichen/releases/latest/download/release.zip'
+with urlopen(ZIPURL) as zipresp:
     with ZipFile(BytesIO(zipresp.read())) as zfile:
-            zfile.extractall(signdir)
+        zfile.extractall(signdir)
 
 # Assumed directory structure for library building: taktischezeichen/svg/ holds
 # several folders, each folder contains one or more svg files representing a #
@@ -51,25 +50,25 @@ special_subdirs = ['gelb', 'rot', 'weiß']
 
 for cwd, dirs, files in walk(path.join(signdir, 'svg'), topdown=True):
     jsonstringlist = []
-    outfilename = ''
-    title = ''
+    OUTFILENAME = ''
+    TITLE = ''
     for file in files:
         # handle subdirs to make sure we have 1 'Schadenskonten' XML file
         if any(x in cwd for x in special_subdirs):
-            outfilename = cwd.lower().split('/')[-2] + '.xml'
-            title = cwd.lower().split('/')[-1] + '_' + file.split('.')[0]
+            OUTFILENAME = cwd.lower().split('/')[-2] + '.xml'
+            TITLE = cwd.lower().split('/')[-1] + '_' + file.split('.')[0]
         else:
-            outfilename = cwd.lower().split('/')[-1] + '.xml'
-            title = file.split('.')[0]
+            OUTFILENAME = cwd.lower().split('/')[-1] + '.xml'
+            TITLE = file.split('.')[0]
 
         b64string = create_b64string(path.join(cwd, file))
-        jsonstringlist.append(create_jsonstring(b64string, title))
+        jsonstringlist.append(create_jsonstring(b64string, TITLE))
 
     xmlstring = f"""<mxlibrary>[{','.join(jsonstringlist)}]</mxlibrary>\n"""
 
     # ensure we only write stuff if we're not in the root directory containing no files
     if files:
-        write_xmlstring_to_file(xmlstring, outfilename)
+        write_xmlstring_to_file(xmlstring, OUTFILENAME)
 
 # clean up previously used sign files
 rmtree(signdir)
